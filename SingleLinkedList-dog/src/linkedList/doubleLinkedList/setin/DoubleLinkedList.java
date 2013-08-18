e4250166504da77e8cfd92f4a835e80161641490
dschoi <setin82@gmail.com>
2013-08-18 14:23:14 +0900

    더블링크드리스트 등록

--- SingleLinkedList-dog/src/linkedList/doubleLinkedList/setin/DoubleLinkedList.java
+++ SingleLinkedList-dog/src/linkedList/doubleLinkedList/setin/DoubleLinkedList.java
@@ -1,10 +1,5 @@
 /**
  * 
- * 노드는 노드의 앞과 노드의 끝을 가지고있어야한다.
- * 
- * 
- * 
- * 
  */
 package linkedList.doubleLinkedList.setin;
 
@@ -13,20 +8,26 @@ package linkedList.doubleLinkedList.setin;
  * 
  */
 public class DoubleLinkedList<T> {
-	private Node<T> node = null;
-	private long size = 0;
+	private Node<T> head = null;
+	private long listSize = 0;
 
 	@SuppressWarnings("hiding")
 	private class Node<T> {
-		private T tData;
-		Node<T> next = null;
+		private T value;
+		Node<T> nextNode = null;
+		Node<T> prevNode = null;
 
 		public Node(T data) {
-			this.tData = data;
+			this.value = data;
 		}
 
 		public T getValue() {
-			return this.tData;
+			return this.value;
+		}
+
+		@Override
+		public String toString() {
+			return getValue().toString();
 		}
 	}
 
@@ -36,13 +37,142 @@ public class DoubleLinkedList<T> {
 	 * @param data
 	 */
 	public void add(T data) {
-		Node<T> dataNode = new Node<T>(data);
-		if (node == null) {
-			node = dataNode;
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
 		} else {
-			getLast().next = dataNode;
+			Node<T> lastNode = getLastNode();
+			lastNode.nextNode = newNode;
+			newNode.prevNode = lastNode;
+
 		}
-		plusSize();
+		increaseSize();
+	}
+
+	/**
+	 * 특정인덱스에 새로운 노드를 삽입
+	 */
+	public void add(final int index, T data) {
+
+		validIndex(index);
+
+		Node<T> preNode = getNode(index - 1);
+
+		insertAfter(preNode, data);
+
+		increaseSize();
+	}
+
+	/**
+	 * 노드 뒤에 해당 노드를 넣음.
+	 */
+	public void insertAfter(Node<T> currentNode, final T data) {
+
+		Node<T> newNode = new Node<T>(data);
+
+		newNode.nextNode = currentNode.nextNode;
+
+		newNode.prevNode = currentNode;
+
+		if (currentNode.nextNode != null) {
+			currentNode.nextNode.prevNode = newNode;
+		}
+
+		currentNode.nextNode = newNode;
+
+		increaseSize();
+
+	}
+
+	
+	/**
+	 * 특정 인덱스의 노드 삭제
+	 * @param index
+	 */
+	public void removeNode(final int index) {
+
+		Node<T> removeNode = getNode(index);
+
+		removeNode(this.head, removeNode);
+
+	}
+
+	/**
+	 * 노드삭제
+	 * 
+	 * @param head
+	 * @param removeNode
+	 */
+	public void removeNode(Node<T> head, Node<T> removeNode) {
+
+		if (head.equals(removeNode)) {
+
+			head = removeNode.nextNode;
+
+			if (head != null) {
+
+				head.prevNode = null;
+
+			}
+
+			removeNode.prevNode = null;
+			removeNode.nextNode = null;
+
+		} else {
+			Node<T> temp = removeNode;
+
+			removeNode.prevNode.nextNode = temp.nextNode;
+
+			if (removeNode.nextNode != null) {
+				removeNode.nextNode.prevNode = temp.prevNode;
+			}
+
+			removeNode.prevNode = null;
+			removeNode.nextNode = null;
+
+		}
+
+	}
+
+	/**
+	 * 특정위치의 노드 탐색
+	 * 
+	 * @param location
+	 * @return
+	 */
+	public Node<T> getNode(final int location) {
+		return getNode(location, this.head);
+	}
+
+	/**
+	 * 특정위치의 노드탐색
+	 * 
+	 * @param location
+	 * @param headNode
+	 * @return
+	 */
+	public Node<T> getNode(final int location, Node<T> headNode) {
+
+		Node<T> current = headNode;
+
+		int index = location;
+
+		while (current != null && (--index) >= 0) {
+			current = current.nextNode;
+		}
+		return current;
+	}
+
+	/**
+	 * 노드 탐색
+	 * 
+	 * @param index
+	 */
+
+	private void validIndex(final int index) {
+		if (index < 0 || this.size() - 1 < index) {
+			throw new IllegalArgumentException("Index is better than List Size");
+		}
 	}
 
 	/**
@@ -50,10 +180,10 @@ public class DoubleLinkedList<T> {
 	 * 
 	 * @return
 	 */
-	private Node<T> getLast() {
-		Node<T> node = this.node;
-		while (node.next != null) {
-			node = node.next;
+	private Node<T> getLastNode() {
+		Node<T> node = this.head;
+		while (node.nextNode != null) {
+			node = node.nextNode;
 		}
 		return node;
 	}
@@ -63,36 +193,24 @@ public class DoubleLinkedList<T> {
 	 * 
 	 * @return
 	 */
-	public long getSize() {
-		return size;
+	public long size() {
+		return listSize;
 	}
 
-	private void plusSize() {
-		this.size++;
+	private void increaseSize() {
+		this.listSize++;
 	}
 
-	private void minusSize() {
-		this.size--;
+	private void decreaseSize() {
+		this.listSize--;
 	}
 
 	/**
 	 * 리스트 비우기.
 	 */
 	public void emptyList() {
-		this.node = null;
-		this.size = 0;
-	}
-
-	/**
-	 * 내용 프린트
-	 */
-	public void printData() {
-		System.out.println("Total Size : " + this.getSize());
-		Node<T> pNode = node;
-		while (pNode != null) {
-			System.out.println(pNode.getValue());
-			pNode = pNode.next;
-		}
+		this.head = null;
+		this.listSize = 0;
 	}
 
 	/**
@@ -101,32 +219,46 @@ public class DoubleLinkedList<T> {
 	 * @param index
 	 * @return
 	 */
-	public boolean delete(int index) {
-		if (this.getSize() - 1 < index) {
-			throw new IllegalArgumentException("Index is better than List Size");
+	public void remove(final int index) {
+
+		validIndex(index);
+		/*
+		 * Node<T> node = this.head; Node<T> pNode = node; if (index == 0) {
+		 * this.head = node.nextNode; } else { for (int i = 0; i <= index; i++)
+		 * { if (i == index) { pNode.nextNode = node.nextNode; break; } pNode =
+		 * node; node = node.nextNode; } }
+		 */
+
+		Node<T> removeNode = getNode(index);
+
+		decreaseSize();
+	}
+
+	@Override
+	public String toString() {
+		Node<T> pNode = this.head;
+		StringBuffer sb = new StringBuffer();
+		String seperator = "|";
+		String seperator2 = "\n";
+		while (pNode != null) {
+			sb.append(pNode).append(seperator).append(seperator2);
+			pNode = pNode.nextNode;
 		}
-		Node<T> node = this.node;
-		int count = 0;
-		boolean checkIndex = false;
-		Node<T> pNode = node;
-		for (int i = 0; i <= index; i++) {
-			if (i == index) {
-				pNode.next = node.next;
-				checkIndex = true;
-				this.size--;
-				break;
-			}
-			pNode = node;
-			node = node.next;
-		}
-		return checkIndex;
+
+		return sb.toString();
 	}
 
 	/**
-	 * 특정위치에 새로운 노드를 삽입
+	 * 내용 프린트
 	 */
-	public void insertAfter() {
-
+	@Deprecated
+	public void printData() {
+		System.out.println("Total Size : " + this.size());
+		Node<T> pNode = head;
+		while (pNode != null) {
+			System.out.println(pNode.getValue());
+			pNode = pNode.nextNode;
+		}
 	}
 
 	/**
@@ -147,36 +279,13 @@ public class DoubleLinkedList<T> {
 		 */
 
 		DoubleLinkedList<String> stringList = new DoubleLinkedList<>();
-		stringList.add("최두석");
-		stringList.add("임정은");
-		stringList.add("정대원");
-		stringList.add("이재경");
-		stringList.add("정연오");
-		stringList.add("이명박");
-		stringList.add("박근혜");
-
-		stringList.printData();
-
-		stringList.delete(5); // 이명박 삭제
-
-		stringList.printData(); // 이명박 삭제됬는지 확인
-
-		try {
-			stringList.delete(6); // 리스트에 없는 인덱스 호출
-		} catch (IllegalArgumentException iex) {
-			System.out.println(iex.getMessage());
-		}
-
-		stringList.printData(); // 삭제됬는지 확인
-
-		//TODO 0번째 리스트를 삭제하는 경우 제대로 동작이 되지 않음.
-		stringList.delete(0);
+		stringList.add("가나다");
+		stringList.add("ABC");
+		stringList.add(1, "abc");
+		System.out.println(stringList.toString());
 		
-		stringList.printData();
-		
-		stringList.emptyList();
-
-		stringList.printData();
+		stringList.removeNode(2);
+		System.out.println(stringList.toString());
 
 	}
 }
