c4373946926aa5c9b90f2953a4339881fca4e064
daewon <blueiur@gmail.com>
2013-12-01 07:36:08 +0000

    quick sort 구현

--- code/algorithm/src/main/sort/daewon/quickSort/QuickSort.java
+++ code/algorithm/src/main/sort/daewon/quickSort/QuickSort.java
@@ -1,10 +1,10 @@
 public class QuickSort {
-  static void partition(int[] arr, int s, int e) {
+  static int partition(int[] arr, int s, int e) {
     int pivot = arr[e];
     int i = s;
     int j = e-1;
 
-    while (i <= j ) {
+    while (i <= j) {
       int l = arr[i];
       int r = arr[j];
 
@@ -15,27 +15,33 @@ public class QuickSort {
       } else if (l < pivot && r < pivot)  {
         i++;
         continue;
+      } else if (l > pivot && r > pivot) {
+        j--;
+        continue;
       } else if (l > pivot && r < pivot) {
         arr[i] = r;
         arr[j] = l;
         i++;
-        j++;
-        continue;
-      } else if (l > pivot && r > pivot) {
         j--;
         continue;
       }
     }
+    arr[e] = arr[i];
+    arr[i] = pivot;
 
-    System.out.println(i);
-    System.out.println(j);
+    return i;
+  }
 
-    arr[e] = arr[j+1];
-    arr[j+1] = pivot;
+  static void sortInRange(int[] arr, int s, int e) {
+    if (e - s < 1)  { return; }
+    int p = partition(arr, s, e);
+    sortInRange(arr, s, p-1);
+    sortInRange(arr, p+1, e);
   }
 
   static void sort(int[] arr) {
-    partition(arr, 0, arr.length-1);
+    int p = partition(arr, 0, arr.length - 1);
+    sortInRange(arr, 0, arr.length - 1);
   }
 
   static void print(int[] arr) {
@@ -51,7 +57,8 @@ public class QuickSort {
   }
 
   public static void main(String[] args) {
-    int[] ints = {23, 15, 38, 62, 19, 16, 42, 25};
+    int[] ints = {1, 2, 3, 23, 15, 38, 62, 19, 16, 42, 25, 12};
+
     print(ints);
     sort(ints);
     print(ints);
